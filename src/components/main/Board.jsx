21807e819f51ae45aa2ca7e526342e616771e245
Deonisteatr <144840698+Deonisteatr@users.noreply.github.com>
2024-06-04 22:27:15 +0300

    Update Board.jsx

--- src/components/main/Board.jsx
+++ src/components/main/Board.jsx
@@ -1,18 +1,19 @@
 import uniqid from "uniqid";
 import { LIST_TYPES, LIST_COPY } from "../../config";
-import List from "./List"
+import List from "./List";
+import { useCallback } from 'react';
 
 const Board = (props) => {
     const { tasks, setTasks } = props;
 
-    const addNewTask = (title, description) => {
+  const addNewTask = useCallback((title, description) => {
         const task = {
             id: uniqid(),
             title,
             status: "backlog",
         }
         setTasks([...tasks, task]);
-    }
+    }, [setTasks, tasks]);
 
     return (
         <div className="board__container">
